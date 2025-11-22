b9bcc9e821dc397182116317f980b71ba650c5f1
lucianoomayer <luciano_mayer@hotmail.com>
2025-11-21 21:54:54 -0300

    Add .env loading for dev profile in main class

--- src/main/java/com/lucianoomayer/scisearch/SciSearchApplication.java
+++ src/main/java/com/lucianoomayer/scisearch/SciSearchApplication.java
@@ -1,13 +1,30 @@
 package com.lucianoomayer.scisearch;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class SciSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SciSearchApplication.class, args);
+        String profile = System.getProperty("spring.profiles.active");
+        if (profile == null) {
+            profile = "dev";
+        }
+
+        if ("dev".equals(profile)) {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry ->
+                    System.setProperty(entry.getKey(), entry.getValue())
+            );
+        }
+
+        SpringApplication app = new SpringApplication(SciSearchApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", profile));
+        app.run(args);
 	}
 
 }
